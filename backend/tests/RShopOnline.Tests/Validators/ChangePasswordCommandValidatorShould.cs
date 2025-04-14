33b19d8dca65d354dff908f81dcec1e4f8382a74
gritsruslan <gritsruslan@gmail.com>
2025-04-14 17:54:30 +0300

    add validators unit tests

---
+++ backend/tests/RShopOnline.Tests/Validators/ChangePasswordCommandValidatorShould.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using RShopAPI_Test.Services.Commands;
+using RShopAPI_Test.Services.Validators;
+
+namespace RShopOnline.Tests.Validators;
+
+public class ChangePasswordCommandValidatorShould
+{
+    private readonly ChangePasswordCommandValidator sut = new();
+    
+    [Fact]
+    public void ReturnSuccess_WhenCommandIsValid()
+    {
+        var validCommand = new ChangePasswordCommand("Password1232", "Password123");
+        var result = sut.Validate(validCommand);
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidCommands))]
+    public void ReturnFailure_WhenCommandIsInvalid(ChangePasswordCommand command)
+    {
+        var result = sut.Validate(command);
+        result.IsValid.Should().BeFalse();
+    }
+
+    public static IEnumerable<object[]> GetInvalidCommands()
+    {
+        var validCommand = new ChangePasswordCommand("Password1231", "Password123");
+        //Password
+        yield return [validCommand with { Password = string.Empty }];
+        yield return [validCommand with { Password = "123" }];
+        yield return [validCommand with { Password = string.Join(string.Empty, Enumerable.Range(0, 100).Select(n => "a")) }];
+        yield return [validCommand with { Password = "password123" }];
+        yield return [validCommand with { Password = "PASSWORD123" }];
+        yield return [validCommand with { Password = "MyPassword" }];
+        //NewPassword
+        yield return [validCommand with { Password = string.Empty }];
+        yield return [validCommand with { Password = "123" }];
+        yield return [validCommand with { Password = string.Join(string.Empty, Enumerable.Range(0, 100).Select(n => "a")) }];
+        yield return [validCommand with { Password = "password123" }];
+        yield return [validCommand with { Password = "PASSWORD123" }];
+        yield return [validCommand with { Password = "MyPassword" }];
+        // Shared
+        yield return [new ChangePasswordCommand(Password: "Password123", NewPassword: "Password123")];
+    }
+}
---
+++ backend/tests/RShopOnline.Tests/Validators/RegistrationCommandValidatorShould.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using RShopAPI_Test.Services.Commands;
+using RShopAPI_Test.Services.Validators;
+
+namespace RShopOnline.Tests.Validators;
+
+public class RegistrationCommandValidatorShould
+{
+    private readonly RegistrationCommandValidator sut = new();
+
+    [Fact]
+    public void ReturnSuccess_WhenCommandIsValid()
+    {
+        var validCommand = new RegistrationCommand("name", "email@email.com", "Password123");
+        var result = sut.Validate(validCommand);
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidCommands))]
+    public void ReturnFailure_WhenCommandIsInvalid(RegistrationCommand command)
+    {
+        var result = sut.Validate(command);
+        result.IsValid.Should().BeFalse();
+    }
+
+    public static IEnumerable<object[]> GetInvalidCommands()
+    {
+        var validCommand = new RegistrationCommand("name", "email@email.com", "Password123");
+        //Email
+        yield return [validCommand with { Email = string.Empty }];
+        yield return [validCommand with { Email = "NotEmail" }];
+        //Name
+        yield return [validCommand with { Name = string.Empty }];
+        yield return [validCommand with { Name = string.Join(string.Empty, Enumerable.Range(0, 100).Select(n => "a")) }];
+        //Password
+        yield return [validCommand with { Password = string.Empty }];
+        yield return [validCommand with { Password = "123" }];
+        yield return [validCommand with { Password = string.Join(string.Empty, Enumerable.Range(0, 100).Select(n => "a")) }];
+        yield return [validCommand with { Password = "password123" }];
+        yield return [validCommand with { Password = "PASSWORD123" }];
+        yield return [validCommand with { Password = "MyPassword" }];
+    }
+}
