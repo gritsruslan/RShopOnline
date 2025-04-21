fd7a19551f208d1b0dcf50a73a6d04b1ba40189a
gritsruslan <gritsruslan@gmail.com>
2025-04-21 12:35:16 +0300

    add e2e tests

---
+++ backend/tests/RShopOnline.E2E/AuthEndpointsShould.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Http.Json;
+using FluentAssertions;
+using RShopAPI_Test.DTOs;
+using Xunit.Abstractions;
+
+namespace RShopOnline.E2E;
+
+public class AuthEndpointsShould(RShopApiApplicationFactory factory, ITestOutputHelper testOutputHelper) : IClassFixture<RShopApiApplicationFactory>
+{
+    [Fact]
+    public async Task RegisterUser()
+    {
+        using var client = factory.CreateClient();
+        using var response = await client.PostAsync("/api/auth/register",
+            JsonContent.Create(new RegistrationRequest("Name", "asdfgasdg123@email.com", "StrongPass123")));
+        
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+    }
+
+
+    [Fact]
+    public async Task NotRegisterUser_WhenUserWithThisEmailAlreadyExists()
+    {
+        using var client = factory.CreateClient();
+        var email = "laskdjflaksdf@pornhub.com";
+        
+        using var response = await client.PostAsync("/api/auth/register",
+            JsonContent.Create(new RegistrationRequest("Name", email ,"StrongPass123")));
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+        
+        using var response2 = await client.PostAsync("/api/auth/register",
+            JsonContent.Create(new RegistrationRequest("Name", email ,"StrongPass123")));
+        testOutputHelper.WriteLine(response2.StatusCode.ToString());
+        response2.Invoking(r => r.EnsureSuccessStatusCode()).Should().Throw<HttpRequestException>();
+    }
+
+    [Fact]
+    public async Task LoginUser()
+    {
+        using var client = factory.CreateClient();
+        var email = "laskdjflaksdf123123@pornhub.com";
+        var password = "0000111popoPo";
+        
+        using var response = await client.PostAsync("/api/auth/register",
+            JsonContent.Create(new RegistrationRequest("Name", email ,password)));
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+
+        using var loginResponse =
+            await client.PostAsync("/api/auth/login", JsonContent.Create(new LoginRequest(email, password)));
+        loginResponse.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task NotLoginUser_WhenNoUserWithThisEmail()
+    {
+        using var client = factory.CreateClient();
+        var email = "dimon_zakon@pornhub.com";
+        var password = "NNNNNNNNNNNNNN123n";
+
+        using var response =
+            await client.PostAsync("/api/auth/login", JsonContent.Create(new LoginRequest(email, password)));
+        
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().Throw<HttpRequestException>();
+    }
+
+    [Fact]
+    public async Task NotLoginUser_WhenPasswordIsIncorrect()
+    {
+        using var client = factory.CreateClient();
+        var email = "myemail@gmaaaail.com";
+        var password = "CorrectPassword52";
+        var incorrectPassword = password + "1";
+        
+        using var response = await client.PostAsync("/api/auth/register", 
+            JsonContent.Create(new RegistrationRequest("Name", email, password)));
+        response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+
+        using var loginResponse = await client.PostAsync("/api/auth/login",
+            JsonContent.Create(new LoginRequest(email, incorrectPassword)));
+        loginResponse.Invoking(r => r.EnsureSuccessStatusCode()).Should().Throw<HttpRequestException>();
+
+    }
+}
---
+++ backend/tests/RShopOnline.E2E/RShopApiApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RShopAPI_Test.Storage;
+using Testcontainers.PostgreSql;
+
+namespace RShopOnline.E2E;
+
+public class RShopApiApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer postgreSqlContainer = new PostgreSqlBuilder().Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var configuration = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>
+        {
+            ["ConnectionStrings:Database"] = postgreSqlContainer.GetConnectionString()
+        }!).Build();
+        builder.UseConfiguration(configuration);
+        base.ConfigureWebHost(builder);
+    }
+    
+    public async Task InitializeAsync()
+    {
+        await postgreSqlContainer.StartAsync();
+
+        var rshopDbContext = new RShopDbContext(
+            new DbContextOptionsBuilder<RShopDbContext>().UseNpgsql(postgreSqlContainer.GetConnectionString()).Options);
+        await rshopDbContext.Database.MigrateAsync();
+    }
+
+    public new async Task DisposeAsync() => await postgreSqlContainer.DisposeAsync();
+}
