d55ac3983b25dfecec289a983b598b22d2157065
gritsruslan <gritsruslan@gmail.com>
2025-04-22 16:47:22 +0300

    add intention resolvers unit tests

---
+++ backend/tests/RShopOnline.Tests/Authorization/ChangePasswordIntentionResolverShould.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using RShopAPI_Test.Core.Enums;
+using RShopAPI_Test.Services.Authentication;
+using RShopAPI_Test.Services.Authorization.Resolvers;
+
+namespace RShopOnline.Tests.Authorization;
+
+public class ChangePasswordIntentionResolverShould
+{
+    private readonly ChangePasswordIntentionResolver sut = new();
+    
+    [Theory]
+    [InlineData(Role.Manager)]
+    [InlineData(Role.Customer)]
+    [InlineData(Role.Admin)]
+    public void ReturnTrue_WhenUserIsNotCustomer(Role role)
+    {
+        var user = new RecognizedUser(Guid.Parse("AB156C01-24B6-41E1-92A9-CCD5F7015894"), role);
+        sut.IsAllowed(user).Should().BeTrue();
+    }
+    
+    [Fact]
+    public void ReturnFalse_WhenUserIsGuest()
+    {
+        var user = RecognizedUser.Guest;
+        sut.IsAllowed(user).Should().BeFalse();
+    }
+}
